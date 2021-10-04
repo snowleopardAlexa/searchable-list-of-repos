e6fd541bef5b5c0d9f0e72d3baef5012429d5498
snowleopardAlexa <alexaslomski@gmail.com>
2021-10-04 13:11:54 -0400

    add div and span in RepoInfo.js

--- searchable-list-repos/src/RepoInfo.js
+++ searchable-list-repos/src/RepoInfo.js
@@ -1,10 +1,19 @@
 const RepoInfo = ({repo}) => {
     return (
         <li className="list-group-item" key={repo.toString()}>
-                  <a className="h5 mb-0 text-decoration-none" href={repo.url}>
+            <div className="d-flex justify-content-between align-items-center">
+                <div className="d-flex flex-column">
+                <a className="h5 mb-0 text-decoration-none" href={repo.url}>
                       {repo.name}
                   </a>
                   <p className="small">{repo.description}</p>
+                </div>
+
+            <span className="px-1 py-1 ms-1 d-inline-block btn btn-sm">
+               {repo.viewerSubscription}
+            </span>
+
+            </div>
         </li>
     );
 };
