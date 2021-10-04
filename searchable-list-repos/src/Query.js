c6c887f0db71df612021cd0bf73072cf66a67011
snowleopardAlexa <alexaslomski@gmail.com>
2021-10-04 14:47:02 -0400

    set up pagination

--- searchable-list-repos/src/Query.js
+++ searchable-list-repos/src/Query.js
@@ -1,27 +1,41 @@
-const githubQuery = (pageCount, queryString) => {
+const githubQuery = (
+    pageCount, 
+    queryString,
+    paginationKeyword,
+    paginationString
+    ) => {
     return {
         query: `
         {
-            viewer {
-                name
-            }
-            search(query: "${queryString}user:snowleopardAlexa sort:updated-desc", type: REPOSITORY, last: ${pageCount}) {
-               repositoryCount 
-               nodes {
-                    ... on Repository {
-                        name
-                        description
-                        id
-                        url
-                        viewerSubscription
-                        licenseInfo {
-                            spdxId
-                        }
-                    }
+          viewer {
+            name
+          }
+          search(query: "${queryString} user:planetoftheweb sort:updated-desc", type: REPOSITORY, ${paginationKeyword}: ${pageCount}, ${paginationString}) {
+            repositoryCount
+            edges {
+              cursor
+              node {
+                ... on Repository {
+                  name
+                  description
+                  id
+                  url
+                  viewerSubscription
+                  licenseInfo {
+                    spdxId
+                  }
                 }
+              }
             }
+            pageInfo {
+              startCursor
+              endCursor
+              hasNextPage
+              hasPreviousPage
+            }
+          }
         }
-       `,
+      `,
     }
 };
     
