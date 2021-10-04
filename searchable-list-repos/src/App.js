5eaa900e9a35b947aa45a877c10c4f590a0e1587
snowleopardAlexa <alexaslomski@gmail.com>
2021-10-04 11:52:56 -0400

    this file is not tracked

--- searchable-list-repos/src/App.js
+++ searchable-list-repos/src/App.js
@@ -1,7 +1,10 @@
 function App() {
   return (
-    <div className="App">
-      
+    <div className="App container mt-5">
+      <h1 className="text-primary">
+        <i className="bi bi-diagram-2-fill"></i>
+         Repos
+      </h1>
     </div>
   );
 }
