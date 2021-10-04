37c38b0922b43c4a6091a26d950e05a92ae70b20
snowleopardAlexa <alexaslomski@gmail.com>
2021-10-04 12:41:26 -0400

    add let

--- searchable-list-repos/src/App.js
+++ searchable-list-repos/src/App.js
@@ -1,28 +1,30 @@
 import github from "./db.js";
-import { useEffect , useState } from "react";
+import { useEffect , useState, useCallback } from "react";
 import query from "./Query.js";
 
 function App() {
 
 let [userName, setUserName] = useState('');
 
-
+const fetchData = useCallback( () => {
+  fetch(github.baseURL, {
+    method: "POST",
+    headers: github.headers,
+    body: JSON.stringify(query),
+  })
+   .then(response => response.json())
+   .then( (data) => {
+     setUserName(data.data.viewer.name)
+   console.log(data);
+ })
+   .catch((err) => {
+    console.log(err);
+ });
+}, []);
 
 useEffect(() => {
-   fetch(github.baseURL, {
-     method: "POST",
-     headers: github.headers,
-     body: JSON.stringify(query),
-   })
-    .then(response => response.json())
-    .then( (data) => {
-      setUserName(data.data.viewer.name)
-    console.log(data);
-  })
-    .catch((err) => {
-    console.log(err);
-  });
-});
+  fetchData();
+}, [fetchData]);
 
   return (
     <div className="App container mt-5">
