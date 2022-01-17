95683f5341b0e57d60a9707ccc12dff920c63efb
snowleopardAlexa <alexaslomski@gmail.com>
2022-01-17 11:29:40 -0500

    add db.js

---
+++ searchable-list-repos/src/db.js
@@ -0,0 +1,10 @@
+const github = {
+    baseURL: "https://api.github.com/graphql",
+    username: "snowleopardAlexa",
+    headers: {
+        "Content-Type": "application/json",
+        Authorization: "bearer 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"
+    },
+};
+
+export default github;
